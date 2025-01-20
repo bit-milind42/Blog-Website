3285025e5c7bbd156d8633c91ccf03137daf07aa
MILIND SINGH <milluk813@gmail.com>
2025-01-20 22:42:06 +0530

    fixed the error

--- Blog/src/components/Header/Header.jsx
+++ Blog/src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ function Header(){
         },
         {
             name: "Login",
-            slug: "/",
+            slug: "/login",
             active: true
         },
         {
@@ -50,9 +50,8 @@ function Header(){
                         {navItems.map((item) =>
                         item.active ? (
                             <li key={item.name}>
-                                <button 
-                                onClick={() => navigate(item.slug)}
-                                className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"> {item.name}</button>
+                                <Link to={item.slug} 
+                                className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"> {item.name}</Link>
 
                             </li>
                         ) : null
